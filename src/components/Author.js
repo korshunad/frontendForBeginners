e6999a811ca9f5745962b9fcd4f3fbd13908a889
HelloTakako <24834291+HelloTakako@users.noreply.github.com>
2019-05-13 22:50:53 -0700

    push test, edited Takako's bio a little bit, deleted white space which is added by conde editor's default settings

--- src/components/Author.js
+++ src/components/Author.js
@@ -5,26 +5,26 @@ const authors = [
   {
     name: 'Ana Makarochkina',
     bio: 'After a long journey in cultural studies and management science, Ana realized that code speaks louder than words. Since that time her major areas of focus are Vim, React, and Tachyons. She likes to write overviews of approaches to frontend development and cats.',
-    imgSrc: '/ana.png'
+    imgSrc:'/ana.png'
   },
   {
     name: 'Takako Nakajima',
     bio: 'Cat person. Gluten-free. Oil painting major, 4 years of design work experience. People in Web development are self-driven, she is an eager learner as well, she likes people in the industry. Her current favorites are JavaScript and SVG animation.',
-    imgSrc: '/takako.jpg'
+    imgSrc:'/takako.jpg'
   },
   {
     name: 'Kate Venedyukhina',
     bio: 'Kate is a graphic designer and digital illustrator. After studying business management at Langara, she decided to pursue a more creative career and learn graphic design and web development. In her free time, she likes baking, drawing, and playing guitar.',
-    imgSrc: '/kate.jpg'
+    imgSrc:'/kate.jpg'
   },
   {
     name: 'Annie Phan',
     bio: 'Annie Phan is a computer science graduate from Kwantlen Polytechnic University where she has studied setting networks in businesses; computer hardware and architecture; web servers and code. Presently, she is at BCIT for New Media and Web Applications programs where she is now learning design principles, front-end coding, and knowledge of Adobe products. She also enjoys late night gaming and bubble tea!',
-    imgSrc: '/annie.jpg'
+    imgSrc:'/annie.jpg'
   }
 ];
 
-const Bio = ({ bio, small, name }) => {
+const Bio = ({bio, small, name}) => {
   if (small) {
     return (
       <p className='mt0'>
@@ -40,11 +40,11 @@ const Bio = ({ bio, small, name }) => {
   }
 }
 
-const Author = ({ imgSrc, name, bio, small }) => (
+const Author = ({imgSrc, name, bio, small}) => (
   <div className={small ? 'pb3 mw7' : 'pb6-l pb4 mw50-l'}>
-    <div className="fl mr4 mb2" style={{ minWidth: '12rem' }}>
+    <div className="fl mr4 mb2" style={{minWidth: '12rem'}}>
       <h2 className={small ? 'tc mt1' : 'mb4 tc mt1'}>{name}</h2>
-      <div className="center br-pill mb4" style={{ backgroundImage: `url(${imgSrc})`, backgroundSize: '100% 100%', backgroundPosition: 'center', height: '9rem', width: '9rem' }}></div>
+      <div className="center br-pill mb4" style={{backgroundImage: `url(${imgSrc})`, backgroundSize: '100% 100%', backgroundPosition: 'center', height: '9rem', width: '9rem'}}></div>
     </div>
     <Bio
       bio={bio}
