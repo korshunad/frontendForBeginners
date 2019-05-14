7460f9b64c4eaf9af5e2c6c2645f3c3a71b701e4
HelloTakako <24834291+HelloTakako@users.noreply.github.com>
2019-05-14 15:25:46 -0700

    add target blank on footer social media links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,16 +3,16 @@ import React from "react";
 export default () => (
   <footer>
     <div className="flex items-center">
-      <a href="https://fb.me/FrontendForBeginners" title="Frontend for beginners Facebook account"  rel="noreferrer" target="_blank">
+      <a href="https://fb.me/FrontendForBeginners" title="Frontend for beginners Facebook account"  rel="noreferrer noopener" target="_blank">
         <img src="/facebook.svg" alt="Frontend for beginners Facebook account"/>
       </a>
-      <a href="https://t.me/FrontendBegins" title="Frontend for beginners Telegram channel"  rel="noreferrer" target="_blank">
+      <a href="https://t.me/FrontendBegins" title="Frontend for beginners Telegram channel"  rel="noreferrer noopener" target="_blank">
         <img src="/telegram.svg" alt="Frontend for beginners Telegram channel"/>
       </a>
-      <a href="https://twitter.com/FrontendBegins" title="Frontend for beginners Twitter account"  rel="noreferrer" target="_blank">
+      <a href="https://twitter.com/FrontendBegins" title="Frontend for beginners Twitter account"  rel="noreferrer noopener" target="_blank">
         <img src="/twitter.svg" alt="Frontend for beginners Twitter account"/>
       </a>
-      <a href="https://www.instagram.com/frontendforbeginners/" title="Frontend for beginners Instagram account"  rel="noreferrer" target="_blank">
+      <a href="https://www.instagram.com/frontendforbeginners/" title="Frontend for beginners Instagram account"  rel="noreferrer noopener" target="_blank">
         <img src="/instagram.svg" alt="Frontend for beginners Instagram account"/>
       </a>
     </div>
