f981c4a83ed92116f54d1e6fc022fe2dbe1d4085
korshunad <korshunadd@gmail.com>
2019-05-25 19:07:27 -0700

    update package.json with more elaborate linting on commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, graphql } from "gatsby"
+import { Link, graphql } from "gatsby";
 
 import Layout from '../components/Layout';
 import FeaturedArticleTile from "../components/FeaturedArticleTile";
@@ -16,23 +16,23 @@ const Featured = ({data}) => {
         link={node.fields.slug}
       />
     ));
-  } else {
-    return (
-      <FeaturedArticleTile
-        title="Welcome to the blog about frontend development!"
-        shortDescription="Modern frontend development is a very dynamic and turbulent sphere. Lots of new frameworks, tools, approaches appear every day. And information about them is often chaotic, unstructured and sometimes incomplete. Here you will find reviews of tools and workflows, tutorials and guides. We focus onto helping you to start and improve your day-to-day frontend development routines and to write clear, maintainable JS code, CSS and HTML."
-        link="/about/"
-      />
-    );
   }
-}
+  return (
+    <FeaturedArticleTile
+      title="Welcome to the blog about frontend development!"
+      shortDescription="Modern frontend development is a very dynamic and turbulent sphere. Lots of new frameworks, tools, approaches appear every day. And information about them is often chaotic, unstructured and sometimes incomplete. Here you will find reviews of tools and workflows, tutorials and guides. We focus onto helping you to start and improve your day-to-day frontend development routines and to write clear, maintainable JS code, CSS and HTML."
+      link="/about/"
+    />
+  );
+  
+};
 
 const Articles = ({articles}) => {
   return (
     <section className={`listicles ${articles.length > 1 ? 'w-100-l pt4' : 'w-40-l'} w-100 ${articles.length === 0 ? 'dn': 'db'}`}>
       <div>
-      <span className="tag">Articles</span>
-      <Link to="/articles/">See all →</Link>
+        <span className="tag">Articles</span>
+        <Link to="/articles/">See all →</Link>
       </div>
       <div className="flex flex-row-ns flex-column w-100 flex-wrap">
         {articles.map(({ node }) => (
@@ -49,14 +49,14 @@ const Articles = ({articles}) => {
       </div>
     </section>
   );
-}
+};
 
 const Tutorials = ({tutorials}) => {
   return (
     <section className={`tutorials ${tutorials.length > 1 ? 'w-100-l' : 'w-100-l'} pt4 w-100 ${tutorials.length === 0 ? 'dn' : 'db'}`}>
       <div>
-      <span className="tag">Tutorials</span>
-      <Link to="/tutorials/">See all tutorials →</Link>
+        <span className="tag">Tutorials</span>
+        <Link to="/tutorials/">See all tutorials →</Link>
       </div>
       <div className="flex flex-row-ns flex-column w-100 flex-wrap">
         {tutorials.map(({ node }) => (
@@ -67,33 +67,32 @@ const Tutorials = ({tutorials}) => {
             link={node.fields.slug}
             imgSrc={node.frontmatter.imgSrc}
             imgAlt={node.frontmatter.imgAlt}
-  limitWidth={true}
+            limitWidth={true}
           />
         ))}
       </div>
     </section>
   );
-}
+};
 
 export default ({data}) => {
   const articles = data.allMarkdownRemark.edges.filter(node => node.node.frontmatter.category === "articles");
   const tutorials = data.allMarkdownRemark.edges.filter(node => node.node.frontmatter.category === "tutorials");
   return (
     <Layout title="Frontend for Beginners" bannerMessage="Hello World!">
-    <main className="pa5-l pa4-m pa3 flex flex-row flex-wrap items-start justify-start">
-    <section className={`${ articles.length > 1 ? 'w-70-ns' : 'w-60-ns' } featured w-100 pr5`}>
-      <Featured data={data}/>
-    </section>
-    <Articles articles={articles}/>
-    <br className={articles ? 'db mt4' : 'dn'}/>
-    <Tutorials tutorials={tutorials}/>
-    <section className="subscribe">
-    </section>
+      <main className="pa5-l pa4-m pa3 flex flex-row flex-wrap items-start justify-start">
+        <section className={`${ articles.length > 1 ? 'w-70-ns' : 'w-60-ns' } featured w-100 pr5`}>
+          <Featured data={data}/>
+        </section>
+        <Articles articles={articles}/>
+        <br className={articles ? 'db mt4' : 'dn'}/>
+        <Tutorials tutorials={tutorials}/>
+        <section className="subscribe" />
       </main>
     </Layout>
 
   );
-}
+};
 
 export const query = graphql`
 query {
@@ -124,4 +123,4 @@ query {
     }
   }
 }
-`
+`;
