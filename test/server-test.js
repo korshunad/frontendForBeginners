2f497d5291299448d64b369a07430c6e7bd95938
korshunad <korshunadd@gmail.com>
2019-05-26 22:37:35 -0700

    setup: now we are building in circle ci as well

--- test/server-test.js
+++ test/server-test.js
@@ -21,24 +21,27 @@ app.use(express.static('public'));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
 */
-app.use(express.static('public'));
-const server = app.listen(3000, () => console.log('Example app listening on port 3000!'));
+
+let server;
 
 describe('puppeteer checks the blog', () => {
+  before(() => {
+    app.use(express.static('public'));
+    server = app.listen(3000, () => console.log('Example app listening on port 3000!'));
+  });
   it('open home page successfully', async () => {
     const browser = await puppeteer.launch({
       headless: true,
       args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
     const page = await browser.newPage();
-    await page.goto('http://127.0.0.1:3000/');
+    await page.goto('http://127.0.0.1:3000/', {waitUntil: 'networkidle2'});
     const bodyHTML = await page.evaluate(() => document.body.innerHTML);
-    console.log(bodyHTML);
+    await browser.close();
 
     const regex = /<\s*span[^>]*>Hello World!<\s*\/\s*span>/g;
     let found = bodyHTML.match(regex);
     expect(bodyHTML).to.equal(null);
-    await browser.close();
   });
   after(() => {
     server.close();
