76a2a2dd548260db154c581b81be83b1de9644d5
korshunad <korshunadd@gmail.com>
2019-07-25 11:21:57 -0700

    test: change port

--- app.js
+++ app.js
@@ -4,4 +4,4 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(4000, () => console.log('Example app listening on port 4000!'));
